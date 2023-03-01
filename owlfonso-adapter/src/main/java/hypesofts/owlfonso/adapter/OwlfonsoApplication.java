eac5a5059fbf466b7473d2c67252fe023975f941
Christophe Schutz <schutz.christophe@gmail.com>
2023-03-01 21:22:54 +0100

    Add missing files

---
+++ owlfonso-adapter/src/main/java/hypesofts/owlfonso/adapter/OwlfonsoApplication.java
@@ -0,0 +1,13 @@
+package hypesofts.owlfonso.adapter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OwlfonsoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OwlfonsoApplication.class, args);
+	}
+
+}
